0c8d567394893b75176e00e317de23ad4d7fd704
d9nich <dan.galaiko@gmail.com>
2020-10-08 15:57:15 +0300

    social_mavr_laplace has been done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from math import factorial, e, sqrt, pi
+import numpy as np
+from math import factorial, e, sqrt, pi, ceil
 from scipy.special import erf
+from scipy.optimize import fsolve
 
 
 def combination(fro: int, to: int) -> float:
@@ -28,21 +30,32 @@ def gauss_function(x: float) -> float:
 
 
 # todo q get from p
-def local_theorem_of_mavr_laplas(k: int, n: int, p: float, q: float) -> float:
+def local_theorem_of_mavr_laplace(k: int, n: int, p: float, q: float) -> float:
     one_divided_by_sqr_of_npq = 1 / sqrt(n * p * q)
     x = (k - n * p) * one_divided_by_sqr_of_npq
     print('x={}'.format(x))
     return one_divided_by_sqr_of_npq * gauss_function(x)
 
 
-def laplassFunction(x: float) -> float:
+def laplace_function(x: float) -> float:
     return erf(x / 2 ** 0.5) / 2
 
 
-def integral_theorem_of_mavr_laplas(n: int, k1: int, k2: int, p: float) -> float:
+def reverse_laplace_function(x: float) -> float:
+    return float(fsolve(lambda value: laplace_function(value) - x, np.ndarray([1]))[0])
+
+
+def integral_theorem_of_mavr_laplace(n: int, k1: int, k2: int, p: float) -> float:
     q = 1 - p
     x1 = (k1 - n * p) / sqrt(n * p * q)
     x2 = (k2 - n * p) / sqrt(n * p * q)
-    return laplassFunction(x2) - laplassFunction(x1)
+    return laplace_function(x2) - laplace_function(x1)
 
-print(integral_theorem_of_mavr_laplas(10_000, 4_850, 5_149, 0.5))
+
+def social_mavr_laplace(alfa: float, betta: float):
+    t_betta = ceil(reverse_laplace_function(betta / 2) * 100) / 100
+    print(t_betta)
+    return int((t_betta ** 2) / 4 / alfa ** 2) + 1
+
+
+print(social_mavr_laplace(0.01, 0.99))
